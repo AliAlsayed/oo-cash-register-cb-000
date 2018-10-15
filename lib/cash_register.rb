13b4cb19d2598cac1c4a910cdc09d054bee05cca
Ali Alsayed <a_alsayed87@hotmail.com>
2018-10-15 15:15:58 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
+  attr_accessor :total, :discount
+  def initialize(discount=nil)
+    @discount = discount
+    @total = 0
+  end
 
+  def add_item(title, price, quantity=1)
+    @total += (price * quantity)
+  end
+
+  def apply_discount
+    if @discount
+      @total -= (@total * (@discount / 100))
+    else
+      return "There is no discount to apply"
+    end
+  end
+end
